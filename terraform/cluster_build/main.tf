17e84eaf97f8cc41ce675d65bc0e1e5825565dfb
Jenn Viau <jenn.charland@gmail.com>
2021-05-11 15:42:25 -0400

    updating node pools create windows and linux locals for node pools added a check for windows nodepool var, defaults to linux node pool only

--- terraform/cluster_build/main.tf
+++ terraform/cluster_build/main.tf
@@ -61,39 +61,43 @@ locals {
       "${module.enabled_google_apis.project_id}=>roles/iam.serviceAccountCreator",
     ]
   }
-
-  // Presets for Windows Node Pool
-  windows_pool = [{
-    name               = format("windows-%s", var.node_pool)
-    min_count          = 0
-    max_count          = 10
-    disk_size_gb       = 100
-    disk_type          = "pd-ssd"
-    image_type         = "WINDOWS_SAC"
-    initial_node_count = 0
-    // Intergrity Monitoring is not enabled in Windows Node pools yet.
-    enable_integrity_monitoring = false
-    enable_secure_boot          = true
-  }]
-
-  // Presets for Linux Node Pool
-  linux_pool = [{
-    name               = format("linux-%s", var.node_pool)
-    min_count          = 1
-    max_count          = 10
-    auto_upgrade       = true
-    node_metadata      = "GKE_METADATA_SERVER"
-    machine_type       = "n1-standard-2"
-    disk_type          = "pd-ssd"
-    disk_size_gb       = 30
-    image_type         = "COS"
-    preemptible        = var.preemptible_nodes ? true : false
-    enable_secure_boot = true
-  }]
-  // Final Node Pool options for Cluster - combines all specified nodepools
-
-  cluster_node_pools = var.windows_nodepool == "true" ? flatten([local.windows_pool, local.linux_pool]) : flatten(local.linux_pool)
-}
+  windows_pool = [
+    {
+      name          = format("linux-%s",var.node_pool)
+      min_count     = 1
+      max_count     = 10
+      auto_upgrade  = true
+      node_metadata = "GKE_METADATA_SERVER"
+      machine_type  = "n1-standard-2"
+      disk_type     = "pd-ssd"
+      disk_size_gb  = 30
+      image_type    = "COS"
+    },
+    {
+      name               = format("windows-%s",var.node_pool)
+      min_count          = 0
+      max_count          = 10
+      disk_size_gb       = 100
+      disk_type          = "pd-ssd"
+      image_type         = "WINDOWS_SAC"
+      initial_node_count = 0
+      // Intergrity Monitoring is not enabled in Windows Node pools yet.
+      enable_integrity_monitoring = false
+    }
+  ]
+  linux_pool = [ {
+      name          = format("linux-%s",var.node_pool)
+      min_count     = 1
+      max_count     = 10
+      auto_upgrade  = true
+      node_metadata = "GKE_METADATA_SERVER"
+      machine_type  = "n1-standard-2"
+      disk_type     = "pd-ssd"
+      disk_size_gb  = 30
+      image_type    = "COS"
+    },
+  ]
+ }
 
 // Enable APIs needed in the gke cluster project
 module "enabled_google_apis" {
@@ -156,4 +160,81 @@ module "kms" {
   owners = [
     "serviceAccount:${local.clu_service_account}",
   ]
+}
+
+module "gke" {
+  depends_on = [
+    module.bastion,
+    module.kms,
+  ]
+  source                  = "terraform-google-modules/kubernetes-engine/google//modules/safer-cluster"
+  version                 = "14.0.1"
+  project_id              = module.enabled_google_apis.project_id
+  name                    = var.cluster_name
+  region                  = var.region
+  config_connector        = var.config_connector
+  network                 = module.vpc.network_name
+  subnetwork              = module.vpc.subnets_names[0]
+  ip_range_pods           = module.vpc.subnets_secondary_ranges[0].*.range_name[0]
+  ip_range_services       = module.vpc.subnets_secondary_ranges[0].*.range_name[1]
+  enable_private_endpoint = var.private_endpoint
+  master_ipv4_cidr_block  = "172.16.0.16/28"
+  master_authorized_networks = [{
+    cidr_block   = var.private_endpoint ? "${module.bastion[0].ip_address}/32" : "${var.auth_ip}/32"
+    display_name = var.private_endpoint ? "Bastion Host" : "Workstation Public IP"
+  }]
+
+  compute_engine_service_account = local.gke_service_account_email
+  database_encryption = [{
+    state    = "ENCRYPTED"
+    key_name = local.database-encryption-key
+  }]
+
+  node_pools =  var.windows_nodepool == "true" ? local.windows_pool : local.linux_pool
+
+  node_pools_oauth_scopes = {
+    all = []
+    (var.node_pool) = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+  }
+  node_pools_labels = {
+    all = {}
+
+    default-node-pool = {
+      default-node-pool = false
+    }
+  }
+
+  node_pools_metadata = {
+    all = {}
+
+    (var.node_pool) = {
+      // Set metadata on the VM to supply more entropy
+      google-compute-enable-virtio-rng = "true"
+      // Explicitly remove GCE legacy metadata API endpoint
+      disable-legacy-endpoints = "true"
+    }
+  }
+}
+
+// Bind the KCC operator Kubernetes service account(KSA) to the 
+// KCC Google Service account(GSA) so the KSA can assume the workload identity users role.
+module "service_account-iam-bindings" {
+  depends_on = [
+    module.gke,
+  ]
+  source = "terraform-google-modules/iam/google//modules/service_accounts_iam"
+
+  service_accounts = [local.kcc_service_account_email]
+  project          = module.enabled_google_apis.project_id
+  bindings = {
+    "roles/iam.workloadIdentityUser" = [
+      "serviceAccount:${module.enabled_google_apis.project_id}.svc.id.goog[cnrm-system/cnrm-controller-manager]",
+    ]
+
+    # "roles/iam.serviceAccountCreator" = [
+    #   "serviceAccount:${module.enabled_google_apis.project_id}.svc.id.goog[cnrm-system/cnrm-controller-manager]",
+    # ]
+  }
 }
